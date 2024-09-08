000e3fa933c7db31ad43fd91701cd29d68be15a6
ssteja294 <ssteja294@gmail.com>
2024-09-08 17:49:04 +0530

    myView.py

--- myView.py
+++ myView.py
@@ -1,14 +1,28 @@
-# Mysql Use of View 
+# Mysql View usage in framework
 
 import mysql.connector
 
 DbConnection = mysql.connector.connect(host='138.68.140.83', user='saiteja', password='Saiteja@123', database='dbSaiteja')
 DbCursor = DbConnection.cursor()
-DbCursor.execute("DROP VIEW IF EXISTS FrameworkTable2")
-DbCursor.execute("CREATE view FrameworkTable2 as SELECT * FROM Item")
-DbCursor.execute("insert into FrameworkTable2 values('TV1397', 'Telivision', 29000, 'LG123', 100)")
+def createView():
+	DbCursor.execute("DROP VIEW IF EXISTS FrameworkTable2")
+	DbCursor.execute("CREATE view FrameworkTable2 as SELECT * FROM Item")
+
+def loadTableColumns():
+	global DbCursor, Fields
+	DbCursor.execute("SHOW COLUMNS FROM FrameworkTable2")
+	Fields = [FieldName[0] for FieldName in DbCursor.fetchall()]
+
+def addRecord():
+	DbCursor.execute("INSERT into FrameworkTable2 values('TV1397', 'Telivision', 29000, 'LG123', 100)")
+    DbConnection.commit()
+
+
 DbCursor.execute("SELECT * from FrameworkTable2")
 Items = DbCursor.fetchall()
 for Item in Items:
-	print(Item)
-
+    for FieldCounter, Field in enumerate(Fields):
+      print(f"{Field}: {Item[FieldCounter]}")
+    print("\n")
+createView()
+loadTableColumns()
